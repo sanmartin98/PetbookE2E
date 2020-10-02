4db85598fefed9097d5cec6ca8115ef2eb2bbb49
mrlottery <mrlotery@gmail.com>
2020-10-01 23:19:08 -0500

    tests

---
+++ src/main/java/co/com/devco/questions/ResultadoFiltroGato.java
@@ -0,0 +1,21 @@
+package co.com.devco.questions;
+
+import net.serenitybdd.screenplay.Actor;
+import net.serenitybdd.screenplay.Question;
+
+import static co.com.devco.userinterfaces.PetbookHomePage.IMAGEN_PERRO_1;
+
+public class ResultadoFiltroGato implements Question<Boolean> {
+    public ResultadoFiltroGato(){
+
+    }
+
+    public static ResultadoFiltroGato noHayPerros(){
+        return new ResultadoFiltroGato();
+    }
+
+    @Override
+    public Boolean answeredBy(Actor actor) {
+        return !IMAGEN_PERRO_1.resolveFor(actor).isVisible();
+    }
+}
---
+++ src/test/resources/features/filtro_gato.feature
@@ -0,0 +1,9 @@
+# language: es
+Característica: Buscar imagenes con filtro de gato
+  Como usuario web
+  Quiero utilizar el filtro de gato
+  Para observar solo las imagenes de los gatos que hay en la galeria
+
+  Escenario: Utilizar filtro de gato
+    Cuando Panchito utilice el filtro de gato
+    Entonces debe ver solo las imagenes de los gatos de la galeria
